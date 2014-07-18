6cdfd017f94f0a5bfbd449b2a34b5094b105af46
Thomas BAUDON <baudon.thomas@gmail.com>
2014-07-18 15:47:44 +0200

    get used lib list

--- src/fr/tbaudon/hxstation/commands/Snapshot.hx
+++ src/fr/tbaudon/hxstation/commands/Snapshot.hx
@@ -11,17 +11,21 @@ class Snapshot extends Command
 {
 	
 	var mPath : String;
+	var mUsedLib : Array<String>;
 	
 	override public function run() {
 		if(mArgs.length > 0){
 			mPath = mArgs[0];
-			if (FileSystem.exists(mPath)){
+			mUsedLib = new Array<String>();
+			if (FileSystem.exists(mPath)) {
+				
+				// get used libs
 				var extension = Path.extension(mPath);
 				switch(extension) {
 					case 'xml' : 
-						parseLimeProj(mPath);
+						getLibsFromXML(mPath);
 					case 'hxml' :
-						parseHxProj(mPath);
+						getLibsFromHXML(mPath);
 				}
 			}else
 				throw new CommandError(mPath + " : no such file");
@@ -30,15 +34,26 @@ class Snapshot extends Command
 			throw new CommandError("not enough arguments");
 	}
 	
-	function parseHxProj(mPath:String) 
+	function getLibsFromHXML(mPath:String) 
 	{
 	}
 	
-	function parseLimeProj(mPath:String) 
+	function getLibsFromXML(mPath:String) 
 	{
-		
 		var fileContent = File.getContent(mPath);
-		trace(fileContent);
+		var xml = Xml.parse(fileContent);
+		for (elem in xml.firstElement().elements()) {
+			if (elem.nodeName == 'haxelib'){
+				var lib = elem.get('name');
+				var version = elem.get('version');
+				if (version != null)
+					lib += ":" + version;
+				else
+					lib += ":xxx";
+				mUsedLib.push(lib);
+				trace(lib);
+			}
+		}
 	}
 	
 	
